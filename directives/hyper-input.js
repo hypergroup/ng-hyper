5f8746b415baf96d94ae7b46cb726588d8f66282
Cameron Bytheway <bytheway.cameron@gmail.com>
2013-11-26 20:09:40 -0700

    support input.title in hyper-input

--- directives/hyper-input.js
+++ directives/hyper-input.js
@@ -14,7 +14,7 @@ package.directive('hyperInput', [
   function() {
     return {
       replace: true,
-      template: '<input data-ng-model="input.value" data-ng-class="{\'ng-hyper-loading\': !input, \'ng-hyper-loaded\': input}" name="{{input.name}}" type="{{input.type}}" placeholder="{{input.prompt || input.placeholder || input.name}}" data-ng-required="input.required">',
+      template: '<input data-ng-model="input.value" data-ng-class="{\'ng-hyper-loading\': !input, \'ng-hyper-loaded\': input}" name="{{input.name}}" type="{{input.type}}" placeholder="{{input.prompt || input.placeholder || input.title || input.name}}" data-ng-required="input.required">',
       scope: {
         input: '=hyperInput'
       }
